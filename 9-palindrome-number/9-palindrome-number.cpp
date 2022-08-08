4b65fa26bb02128d2b605e33f4e1aa2c680fdb79
Varcos <63259133+varcoss@users.noreply.github.com>
2022-08-07 23:51:39 -0700

    Time: 19 ms (67.36%), Space: 5.9 MB (31.94%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    bool isPalindrome(int x) 
+    {
+        int digit[1000] = {};
+        int i = 0;
+        bool palindrone = false;
+        int tempNum = x;
+        
+
+        if(x < 10 && x >= 0)
+        {
+            return true;
+        }
+        
+        if(x < -10 && x < 0)
+        {
+            return false;
+        }
+        
+        if(x < 0)
+        {
+            return false;
+        }
+        
+        while (x > 0)
+        {
+            digit[i] = x % 10;
+            x /= 10;
+            i++;
+        }
+        
+        if(tempNum % 2 == 0)
+        {
+            int indexMinusOne = i - 1;
+            int indexInHalf = indexMinusOne / 2;
+            
+            for(int o = 0; o <= indexInHalf; o++)
+            {
+                if(digit[o] != digit[indexMinusOne]){
+                    return false;
+                }
+                indexMinusOne--;
+            }
+        }
+        
+        if(tempNum % 2 == 1)
+        {
+            int indexMinusOne = i - 1;
+            int indexInHalf = (indexMinusOne-1) / 2;
+ 
+            
+            for(int o = 0; o <= indexInHalf; o++)
+            {
+                if(digit[o] != digit[indexMinusOne]){
+                    return false;
+                }
+                indexMinusOne--;
+            }
+        }
+        
+        return true;
+    }
+};
